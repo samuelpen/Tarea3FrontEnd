2bc5072d8cb05942d1c339a8ee49bf1e4d142bfd
Samuel Peña <samuel_pena@hotmail.es>
2019-06-26 17:06:17 -0400

    Commit con todos los ViewModels terminados

---
+++ Tarea3SamuelPeña/AppEquipos/AppEquipos/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEquipos.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
---
+++ Tarea3SamuelPeña/AppEquipos/AppEquipos/ViewModels/TeamViewModel.cs
@@ -0,0 +1,101 @@
+﻿using AppEquipos.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AppEquipos.Services;
+using Xamarin.Forms;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+namespace AppEquipos.ViewModels
+{
+    
+
+        class TeamViewModel : BaseViewModel
+        {
+            #region Services
+
+            private ApiService apiService;
+            #endregion
+
+
+            #region Attributes
+
+            private ObservableCollection<Team> team;
+            private bool isRefreshing;
+            #endregion
+
+
+            #region Properties
+            public ObservableCollection<Team> Student
+            {
+                get { return this.team; }
+                set { SetValue(ref this.team, value); }
+            }
+
+            public bool IsRefreshing
+            {
+                get { return this.isRefreshing; }
+                set { SetValue(ref this.isRefreshing, value); }
+            }
+            #endregion
+
+
+
+            #region Constructors
+
+            public TeamViewModel()
+            {
+                this.apiService = new ApiService();
+                this.LoadTeams();
+            }
+
+            #endregion
+
+
+            #region Methods
+            private async void LoadTeams()
+            {
+                this.IsRefreshing = true;
+                var connection = await this.apiService.CheckConnection();
+                if (!connection.IsSuccess)
+                {
+                    this.IsRefreshing = false;
+                    await Application.Current.MainPage.DisplayAlert(
+                       "Error", connection.Message,
+                       "Aceptar");
+                    return;
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                }
+
+                var response = await this.apiService.GetList<Team>(
+                    "https://www.scorebat.com",
+                    "/video-api",
+                    "/v1");
+                if (!response.IsSuccess)
+                {
+                    this.IsRefreshing = false;
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Error", response.Message,
+                        "Aceptar");
+                    return;
+                }
+                var list = (List<Team>)response.Result;
+                this.Student = new ObservableCollection<Team>(list);
+                this.IsRefreshing = false;
+            }
+            #endregion
+
+            #region Commands
+            public ICommand RefreshCommand
+            {
+                get
+                {
+                    return new RelayCommand(LoadTeams);
+
+                }
+            }
+            #endregion
+        }
+
+
+    
+}
