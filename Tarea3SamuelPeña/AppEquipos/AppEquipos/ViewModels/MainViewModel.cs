8e809a943f844825f91df5d4299c972797eda5ce
Samuel Peña <samuel_pena@hotmail.es>
2019-06-26 17:09:47 -0400

    Commit con ViewModels

--- Tarea3SamuelPeña/AppEquipos/AppEquipos/ViewModels/MainViewModel.cs
+++ Tarea3SamuelPeña/AppEquipos/AppEquipos/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppEquipos.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,46 @@ namespace AppEquipos.ViewModels
 {
     class MainViewModel
     {
+        #region Properties
+        public TokenResponse Token
+        {
+            get; set;
+        }
+
+        #endregion
+
+        #region ViewModels
+        public TeamViewModel Login
+        {
+            get; set;
+        }
+        public TeamViewModel Team
+        {
+            get; set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new TeamViewModel();
+
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+
+            }
+            return instance;
+        }
+
+        #endregion
     }
 }
