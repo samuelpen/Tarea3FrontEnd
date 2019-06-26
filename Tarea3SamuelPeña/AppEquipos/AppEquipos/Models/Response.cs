d0d59519d2c906e1dc35d5ae20b0b17e7c821112
Samuel Peña <samuel_pena@hotmail.es>
2019-06-26 16:54:01 -0400

    Commit con clases Token y ApiService

---
+++ Tarea3SamuelPeña/AppEquipos/AppEquipos/Models/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEquipos.Models
+{
+    public class Response
+    {
+
+        public bool IsSuccess
+        {
+            get;
+            set;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public object Result
+        {
+            get;
+            set;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Tarea3SamuelPeña/AppEquipos/AppEquipos/Models/Team.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEquipos.Models
+{
+    public class Team
+    {
+        [JsonProperty(PropertyName = "tame")]
+        public string Title { get; set; }
+        [JsonProperty(PropertyName = "embed")]
+        public string Embed { get; set; }
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+        [JsonProperty(PropertyName = "thumbnail")]
+        public string Thumbnail { get; set; }
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+
+    }
+
+
+
+
+
+}
